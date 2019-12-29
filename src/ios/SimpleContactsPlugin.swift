0e1b09f25583d7042900b16c36dcc5f941a9a677
Hunni.Devteam <hunni.devteam@gmail.com>
2019-12-30 00:36:55 +0900

    add GetAllContacts Method

--- src/ios/SimpleContactsPlugin.swift
+++ src/ios/SimpleContactsPlugin.swift
@@ -1,3 +1,5 @@
+import Contacts
+
 @objc(SimpleContactsPlugin) class SimpleContactsPlugin : CDVPlugin {
     @objc(echo:)
     func echo(command: CDVInvokedUrlCommand) {
@@ -9,19 +11,16 @@
 
         if msg.count > 0 {
             /* UIAlertController is iOS 8 or newer only. */
-            let toastController: UIAlertController = 
-                UIAlertController(
-                    title: "", 
-                    message: msg, 
-                    preferredStyle: .alert
-                )
-
+            let toastController: UIAlertController = UIAlertController(
+                title: "", 
+                message: msg,
+                preferredStyle: .alert
+            )
             self.viewController?.present(
                 toastController, 
                 animated: true, 
                 completion: nil
             )
-
 //            let duration = Double(NSEC_PER_SEC) * 3.0
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 toastController.dismiss(
@@ -40,5 +39,33 @@
             pluginResult, 
             callbackId: command.callbackId
         )
+    }
+    
+    @objc(getAllContacts:)
+    func getAllContacts(command: CDVInvokedUrlCommand) {
+        let contactStore = CNContactStore()
+        var contacts = [CNContact]()
+        let keys = [
+            CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+            CNContactPhoneNumbersKey,
+            CNContactEmailAddressesKey
+        ] as [Any]
+        let request = CNContactFetchRequest(keysToFetch: keys as! [CNKeyDescriptor])
+        do {
+            try contactStore.enumerateContacts(with: request){
+                    (contact, stop) in
+                // Array containing all unified contacts from everywhere
+                contacts.append(contact)
+                for phoneNumber in contact.phoneNumbers {
+                    let number = phoneNumber.value
+                    let label = phoneNumber.label
+                    let localizedLabel = CNLabeledValue<CNPhoneNumber>.localizedString(forLabel: label!)
+                    print("\(contact.givenName) \(contact.familyName)\n tel:\(localizedLabel) -- \(number.stringValue),\n email: \(contact.emailAddresses)\n")
+                }
+            }
+            print(contacts)
+        } catch {
+            print("unable to fetch contacts")
+        }
     }
 }
--- www/SimpleContactsPlugin.js
+++ www/SimpleContactsPlugin.js
@@ -1,13 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.echo = function(arg0, success, error) {
-    exec(success, error, "ModusEchoSwift", "echo", [arg0]);
+    exec(success, error, "SimpleContactsPlugin", "echo", [arg0]);
 };
 
-exports.echojs = function(arg0, success, error) {
-	if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-		success(arg0);
-	} else {
-		error('Empty message!');
-	}
+exports.getAllContacts = function(arg0, success, error) {
+	exec(success, error, "SimpleContactsPlugin", "getAllContacts", [arg0]);
 };
